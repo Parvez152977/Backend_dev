4663f46dafdd74bc69a33589e91c15fe0b1794e1
Parvez152977 <parvezalam12977@gmail.com>
2026-03-13 14:26:50 +0600

    simple auth api completed

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -70,7 +70,28 @@ const loginUser = async (req, res) => {
         });
     }
 }
+
+const logoutUser = async (req, res) => {
+    try {
+        const { email} = req.body;
+        const user = await User.findOne({
+            email
+        });
+        if (!user) return res.status(404).json({
+            message: "User not found"
+        });
+
+        res.status(200).json({
+            message: "Logout Successful"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+}
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -52,4 +52,5 @@ userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
+
 export const User = mongoose.model("User", userSchema);
