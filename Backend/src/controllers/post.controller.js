45cbe9b4c4d0836bda30f8fa3e91e9f99159e240
Parvez152977 <parvezalam12977@gmail.com>
2026-03-14 11:27:03 +0600

    crud api (updatePost and deletePost ) created and updated

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -35,7 +35,58 @@ const getPosts = async (req, res) => {
         });
     }
 }
+
+//update posts
+const updatePost = async (req, res) => {
+    try {
+        //basic validation if the field is empty
+
+        //{name: x , description : y, age: z}->[name,description,age]
+        if (Object.keys(req.body).length == 0) {
+            return res.status(400).json({
+                message: "No data for update"
+            });
+        }
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found"
+            });
+        };
+        res.status(200).json({
+            message: "Post updated successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+}
+
+//Delete post
+const deletePost = async (req, res) => {
+    try {
+        const postDeleted = await Post.findByIdAndDelete(req.params.id);
+        if (!postDeleted) {
+            return res.status(404).json({
+                message: "Post not found"
+            })
+        }
+        res.status(200).json({
+            message: "Post deleted successfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+}
+
 export {
     createPost,
-    getPosts 
+    getPosts,
+    updatePost,
+    deletePost
 }
